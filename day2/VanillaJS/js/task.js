2f3387186a71d7b1a771a9050e502b7beae3ac1d
齋藤 和人 <s01719@CG1800.local>
2018-04-18 17:58:38 +0900

    同盟のタスクが保持できるようになった

--- day2/VanillaJS/js/task.js
+++ day2/VanillaJS/js/task.js
@@ -1,33 +1,49 @@
-class task{
+//タスク管理クラス
+
+//タスクの一意性を管理するクロージャ
+
+function getMaxIndexFromList(){
+    var max = 0;
+    if(taskList == null){
+        return 0;
+    }
+    taskList.forEach(function(task){if(task.index > max){max = task.index;}});
+    return max;
+}
+
+class Task{
     constructor(title){
         this.title = title;
-        this.cleared = false;
+        this.checked = false;
+        this.index = getMaxIndexFromList() + 1;
     }
 }
 
-function getCheckButton(titleText)
+var taskList = [];
+
+//各フォーム生成
+function getCheckButton(task)
 {//チェックボタンを作る
     var item = document.createElement('input');
     item.setAttribute("type","checkbox");    
-    item.setAttribute("value","1");
-    item.setAttribute("name",titleText);
+    item.setAttribute("value",task.checked);
+    item.setAttribute("name",task.title);
+    item.setAttribute("task-index",task.index);
     item.id = "task-checkButton";
-
-    // item.addEventListener("click",function(){console.log(term);});
-    // item.addEventListener("dblclick",function(){console.log("Edit" + term);});
-
+    item.className =  "task-elements";
     return item;
 }
 
-function getTitleTextForm(titleText)
+function getTitleTextForm(task)
 {//中央のタイトルフォームを作る。ダブルクリック時の対応なども
     var item = document.createElement('input');
     item.setAttribute("type","text");
-    item.setAttribute("value",titleText);
-    item.setAttribute("name",titleText);
+    item.setAttribute("value",task.title);
+    item.setAttribute("name",task.title);
+    item.setAttribute("task-index",task.index);
     item.id = "task-text";
     // item.setAttribute("readOnly","true");
-    item.className = "task-text";
+    item.className = "task-elements";
 
     item.addEventListener("dblclick",function(){
        if(item.getAttribute("readOnly")){
@@ -38,14 +54,31 @@ function getTitleTextForm(titleText)
     return item;
 }
 
+function getDeleteButton(task)
+{
+    var item = document.createElement('input');
+    item.setAttribute("type","button");
+    item.setAttribute("name",task.name);
+    item.setAttribute("onclick","deleteElement(this)");
+    item.setAttribute("task-index",task.index);
+    item.id = "task-removeButton";
+    item.className =  "task-elements";
+    item.innerHTML = "x";
+
+    // return wrapWithTdElement(item);
+    return item;
+}
+
+// 指定した要素を削除する。
+// Bug: 同名のタスクも消される
 function deleteElement(elem){
     //elemから、タスク名を取り出す。
-    var text = elem.getAttribute("name");
-    console.log(text);
+    var index = elem.getAttribute("task-index");
+    console.log(index);
     
     var res = [];
     taskList.forEach(function(task){
-        if(task !== text){
+        if(task.index != index){
             res.push(task);
         }
     });
@@ -53,48 +86,30 @@ function deleteElement(elem){
     saveTaskListToLocalStrage();
     location.reload();    
 }
-//唯一idで区別しないといけない
 
-var indexCounter = 0;
 
-function getDeleteButton(titleText)
-{
-    var item = document.createElement('input');
-    item.setAttribute("type","button");
-    item.setAttribute("name",titleText);
-    item.setAttribute("onclick","deleteElement(this)");
-    item.id = "task-removeButton";
-    item.innerHTML = "x";
-
-    // return wrapWithTdElement(item);
-    return item;
-}
-
-function getTaskForm(row, titleText){
-    // var item = document.createElement("tr");
-    var item1 = row.insertCell(-1);
-    var item2 = row.insertCell(-1);
-    var item3 = row.insertCell(-1);
-    // item2.setAttribute("width","500px");
-
-    item1.appendChild(getCheckButton(titleText));
-    item2.appendChild(getTitleTextForm(titleText));
-    item3.appendChild(getDeleteButton(titleText));
-    // return item;
-}
-
+//ローカルストレージへの読み書き関数
 function loadTaskListFromLocalStrage(){
     var rawData = localStorage.getItem("data");
     taskList = JSON.parse(rawData);
     console.log(taskList);
     if(taskList == null){taskList = []};
 }
-
 function saveTaskListToLocalStrage(){
+
+    // taskList.forEach(function(task){
+    //     var dat = JSON.stringify(task)
+    // });
+
     var data = JSON.stringify(taskList);
-    localStorage.setItem("data",data);    
+    console.log(data);
+    
+    localStorage.setItem("data",data);
+    console.log("Save:");
+    
 }
 
+//エンターキーが押されたら、新しいタスクを追加する。
 function submitNewTask()
 {
     if(window.event.keyCode==13){
@@ -104,13 +119,26 @@ function submitNewTask()
         if(taskName === ""){
             return false;
         }
-        taskList.push(taskName);
+        taskList.push(new Task(taskName));
         saveTaskListToLocalStrage();
         form.submit();
     }
 }
 
+//タスクテーブルを生成する。
 function makeTable(taskList){
+    function getTaskForm(row, titleText){
+        // var item = document.createElement("tr");
+        var item1 = row.insertCell(-1);
+        var item2 = row.insertCell(-1);
+        var item3 = row.insertCell(-1);
+        // item2.setAttribute("width","500px");
+    
+        item1.appendChild(getCheckButton(titleText));
+        item2.appendChild(getTitleTextForm(titleText));
+        item3.appendChild(getDeleteButton(titleText));
+        // return item;
+    }    
 
     var table = document.createElement("table");
     table.id = "form-box";
@@ -126,6 +154,7 @@ function makeTable(taskList){
 
 window.onload = function(){
     //実際はここでローカルストレージから読み込む
+    // this.localStorage.clear();
     loadTaskListFromLocalStrage();
     makeTable(taskList);
 }
